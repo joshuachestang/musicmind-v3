78b6327720cd80244c56a9a75c12570dbd3da219
Joshua Chestang <joshua.chestang@gmail.com>
2012-12-17 01:11:18 -0600

    changed repository name

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:joshuachestang/#{application}.git"
+set :repository, "git@github.com:joshuachestang/musicmind-v3.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
